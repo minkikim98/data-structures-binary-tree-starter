eb30083ce0b8b41dc9a2f703f06fb29fbb7e7815
Tristan San Juan <tristansj88@gmail.com>
2021-02-02 14:06:59 -0700

    finished leetcode105

--- problems/05-leet-code-105.js
+++ problems/05-leet-code-105.js
@@ -2,7 +2,22 @@
 //  https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
 const { TreeNode } = require('./01_tree_node.js');
-
 function buildTree(preorder, inorder) {
+  if (preorder.length === 0 && inorder.length === 0) return null;
 
+  let rootVal = preorder[0];
+  let root = new TreeNode(rootVal);
+
+  let midIdx = inorder.indexOf(rootVal);
+
+  let leftIndorder = inorder.slice(0, midIdx);
+  let rightInorder = inorder.slice(midIdx + 1);
+
+  let leftPreoder = preorder.filter((value) => leftIndorder.includes(value));
+  let rightPreoder = preorder.filter((value) => rightInorder.includes(value));
+
+  root.left = buildTree(leftPreoder, leftIndorder);
+  root.right = buildTree(rightPreoder, rightInorder);
+
+  return root;
 }
